04174b95d5be8c62271580d0e80b93f5fdc7bbcd
Rochana sadila Weerasinghe <163114128+rochana-sadila@users.noreply.github.com>
2024-03-29 19:41:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+id = input ("Enter bot ID(APPLICATION ID) ")
+print ("https://discord.com/api/oauth2/authorize?client_id="+id+"&permissions=0&scope=bot%20applications.commands")
+print ("2022-2023 DEX")
